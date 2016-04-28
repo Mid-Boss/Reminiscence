ed6acb1f2501fec51b9c1c85c1ea277b285c1883
quatrojohn <jasmz6@mst.edu>
2016-04-28 17:17:38 -0500

    MATLAB enemy stat scipt/functions, Illista disappears from Skaylon after Telenor

---
+++ MATLAB/avglvl.m
@@ -0,0 +1,20 @@
+function [ ] = avglvl( lvl )
+%avglvl Function for testing purposes only. Prints the average class stats
+%at a specified level to the command window
+
+load avgClassStats.mat;
+avgStats.lvl = lvl;
+avgStats.mhp = avg.mhp(lvl);
+avgStats.mmp = avg.mmp(lvl);
+avgStats.att = avg.att(lvl);
+avgStats.def = avg.def(lvl);
+avgStats.mat = avg.mat(lvl);
+avgStats.mdf = avg.mdf(lvl);
+avgStats.agi = avg.agi(lvl);
+avgStats.luk = avg.luk(lvl);
+
+% Ignore MATLAB's complaining and just print it the easy way
+avgStats
+
+end
+
---
+++ MATLAB/calcStats.m
@@ -0,0 +1,26 @@
+function [ foo ] = calcStats( foo,mhp,mmp,att,def,mat,mdf,agi,luk )
+%calcStats Given an enemy structure, foo, with a level field, .lvl,
+%calculates the the enemy's stats based on the percentages mhp, mmp, ...,
+%luk of the average class stats
+
+load avgClassStats.mat;
+avg = avg;
+
+lvl = foo.lvl;
+foo.mhp = myround(avg.mhp(lvl)*mhp/100,mhp);
+foo.mmp = myround(avg.mmp(lvl)*mmp/100,mmp);
+foo.att = myround(avg.att(lvl)*att/100,att);
+foo.def = myround(avg.def(lvl)*def/100,def);
+foo.mat = myround(avg.mat(lvl)*mat/100,mat);
+foo.mdf = myround(avg.mdf(lvl)*mdf/100,mdf);
+foo.agi = myround(avg.agi(lvl)*agi/100,agi);
+foo.luk = myround(avg.luk(lvl)*luk/100,luk);
+
+    function [bar] = myround(a,b)
+        bar = round(a);
+        if(bar == 0 && b ~= 0)
+            bar = 1;
+        end
+    end
+end
+
